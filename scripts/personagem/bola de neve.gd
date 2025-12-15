6e5026f69ae57bad9c31ffe4c3e1503f02bf0f8d
JFVIANA10 <jfvyana@gmail.com>
2025-12-15 10:22:39 -0300

    player funcional

--- scripts/personagem/bola de neve.gd
+++ scripts/personagem/bola de neve.gd
@@ -1,11 +1,26 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var speed: float = 300.0
+var direction: Vector2 = Vector2.DOWN
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physicis_process(delta):
+	position += direction * speed * delta
+	
+func _ready():
+	connect("body_entered", _on_body_entered)
+
+func _on_body_entered(body:Node2D):
+	if body.is_in_group("Hurtbox"):
+		body.anima_morta()
+		queue_free()
+		
+		
+func _on_visibility_notifiar_sreen_exited():
+	queue_free()
+	
+func _on_self_destruct_timer_tikmeout() -> void:
+	queue_free()
+
+func _on_area_entered(_area: Area2D) -> void :
+	queue_free()
