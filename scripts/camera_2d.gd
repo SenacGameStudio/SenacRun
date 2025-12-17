63ee51a74fb57d7b0ce16b2a5697da8f8d77ea01
Mrink0zz <dmaurinkxb@gmail.com>
2025-12-17 08:28:04 -0300

    Final Commit

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -1,22 +1,22 @@
 extends Camera2D
 
-var alvo:Node2D
+var alvo: Node2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	buscar_alvo() # Replace with function body.
-	zoom.x = 0.3
-	zoom.y = 0.3
+	buscar_alvo()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	position=alvo.position
-
+	position = alvo.position
+	
 func buscar_alvo():
-	var nodes=get_tree().get_nodes_in_group("player")
+	var nodes = get_tree().get_nodes_in_group("player")
+	
 	if nodes.size() == 0:
-		push_error("no player nao foi encontrado")
+		push_error("NO player nao foi encontrado")
 		return
 		
-		
-	alvo = nodes[0]
+	alvo = nodes [0]
