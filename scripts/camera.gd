18de25ac07706da00d3c5cd6201088caaa04a2c4
Patrick Son <pmacedo1106@gmail.com>
2025-12-17 15:10:31 -0300

    Adicionadas as cenas

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -5,6 +5,7 @@ var alvo: Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	buscar_alvo() # Replace with function body.
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
